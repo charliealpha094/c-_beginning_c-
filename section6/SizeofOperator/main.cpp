160f75161ddb8baf9c9795a209d99e741e2fb550
Carlos <carlosamaral94@gmail.com>
2024-02-23 21:43:32 +0000

    SizeOf Operator

---
+++ section6/SizeofOperator/main.cpp
@@ -0,0 +1,59 @@
+// Done by Carlos Amaral (2024/02/21)
+
+// The sizeof operator
+
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    cout << "sizeof information" << endl;
+    cout << "==========================" << endl;
+
+    cout << "char: " << sizeof(char) << " bytes." << endl;
+    cout << "int: " << sizeof(int) << " bytes." << endl;
+    cout << "unsigned int: " << sizeof(unsigned int) << " bytes." << endl;
+    cout << "short: " << sizeof(short) << " bytes." << endl;
+    cout << "long: " << sizeof(long) << " bytes." << endl;
+    cout << "long long: " << sizeof(long long) << " bytes." << endl;
+
+
+    cout << "==========================" << endl;
+
+    cout << "float: " << sizeof(float) << " bytes." << endl;
+    cout << "double: " << sizeof(double) << " bytes." << endl;
+    cout << "long double: " << sizeof(long double) << " bytes." << endl;
+
+
+    // use values defined in <climits>
+    cout << "==========================" << endl;
+
+    cout << "Minimum values:" << endl;
+    cout << "char: " << CHAR_MIN << endl;
+    cout << "int: " << INT_MIN << endl;
+    cout << "short: " << SHRT_MIN << endl;
+    cout << "long: " << LONG_MIN << endl;
+    cout << "long long: " << LLONG_MIN << endl;
+
+
+    // sizeof can also be used with variable names
+    cout << "==========================" << endl;
+
+    cout << "sizeof using variable names" << endl;
+    int age {21};
+    cout << "age is " << sizeof(age) << " bytes" << endl;
+    // or
+    cout << "age is " << sizeof age << " bytes" << endl;
+
+    double wage {22.24};
+    cout << "wage is " << sizeof(wage) << " bytes" << endl;
+    // or
+    cout << "wage is " << sizeof(wage) << " bytes" << endl;
+
+
+    return 0;
+
+
+}
